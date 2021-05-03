fa428cbe8d19bb27b73b98a714b9b23b67c71a59
Thomas-Quig <trquig1@gmail.com>
2021-05-02 21:28:59 -0500

    Version 1.6.1, no functional changes just a comment

--- src/arithFunc.js
+++ src/arithFunc.js
@@ -65,3 +65,62 @@ module.exports = {
   
   
 }
+
+/**
+ * export function vulnerable()
+  {
+    console.log('VULNERABLE CODE EXECUTED!!!');
+  }
+
+  export function funcOne(i1){
+    //vulnerable()
+    return i1 + 2;
+  }
+
+  export function funcTwo(i1){
+    return i1 * 3;
+  }
+
+  export function funcThree(i1,i2){
+    //vulnerable();
+    return i1 + i2 - 4;
+  }
+
+  export function funcFour(i1){
+    if(i1 == 1337)
+        return 1337;
+    return 7331;
+  }
+
+  export function funcFive(i1,i2,i3){
+    if (i1 == i2 && i2 != i3)
+        return i1 + i3;
+    return i1 + i2;
+  }
+
+  export function funcSix(i1){
+    return i1 * i1 * i1;
+  }
+
+  export function funcSeven(i1){
+    //vulnerable();
+    return 0;
+  }
+
+  export function funcEight(i1,i2,i3,i4){
+    return i1 * i2 * i3 * i4;
+  }
+
+  export function funcNine(i1,i2,i3){
+    vulnerable();
+    return i2 * (i3 - i1);
+  }
+
+  export function funcTen(i1){
+    return i1;
+  }
+
+  export function funcEleven(i1){
+    return (i1 + 7) * 11;
+  }
+ */
